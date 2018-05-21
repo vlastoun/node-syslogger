3b236a967acfabbe753c97d5747e98f87489e82f
Vlastimil Sadilek <vsadilek@linuxbox.cz>
2018-05-21 15:04:05 +0200

    Feat: tests

--- src/Logger.test.ts
+++ src/Logger.test.ts
@@ -1,12 +1,12 @@
-import Logger from "./Logger";
+import Logger from './Logger';
 
-describe("Logger", () => {
-  it("should instanciate", () => {
-    const logger = new Logger("test");
+describe('Logger', () => {
+  it('should instanciate', () => {
+    const logger = new Logger('test');
     expect(logger).toBeDefined();
   });
-  it("should have methods", () => {
-    const logger = new Logger("test");
+  it('should have methods', () => {
+    const logger = new Logger('test');
     expect(logger.emerg).toBeDefined();
     expect(logger.alert).toBeDefined();
     expect(logger.crit).toBeDefined();
@@ -17,15 +17,15 @@ describe("Logger", () => {
     expect(logger.debug).toBeDefined();
   });
 
-  it("should parse object", () => {
-    const logger = new Logger("test");
+  it('should parse object', () => {
+    const logger = new Logger('test');
     const parsedResult = '{"test":"123"}wow';
-    const result = logger.parseObject("wow", { test: "123" });
+    const result = logger.parseObject('wow', { test: '123' });
     expect(result).toEqual(parsedResult);
   });
 
-  it("should set correctly parser", () => {
-    const logger = new Logger("test");
+  it('should set correctly parser', () => {
+    const logger = new Logger('test');
 
     function parser(object) {
       return `${object.info}${object.data}`;
@@ -34,12 +34,19 @@ describe("Logger", () => {
     logger.setFormat(parser);
 
     const testObject = {
-      info: "wow",
-      data: "123"
+      info: 'wow',
+      data: '123',
     };
 
-    expect(logger.parseObject("inputstring", testObject)).toEqual(
-      "wow123inputstring"
-    );
+    expect(logger.parseObject('aaa', testObject)).toEqual('wow123aaa');
+  });
+
+  it('should parse string in defalut', () => {
+    const logger = new Logger('test');
+    expect(logger.parseObject('aaa', 'bbb')).toEqual('"bbb"aaa');
+  });
+
+  it('should stringify string', () => {
+    expect(JSON.stringify('wow')).toEqual('"wow"');
   });
 });
